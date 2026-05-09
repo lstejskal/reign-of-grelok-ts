65e323ba2afb96836e038b8101ccb90adcd39ebb
Lukas Stejskal <lucastej@gmail.com>
2026-05-09 23:32:45 +0200

    Replace ruby Array extension toSentence

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
 
+import { toSentence } from './utils/toSentence.js'; 
+
 const version: string = '1.0.0';
 
 console.log('REIGN OF GRELOK', `v${version}`);
+
+console.log(
+    toSentence(
+        [ 'apple', 'sword', 'ladder'],
+        { prepend: 'You carry ' }
+    )
+);
---
+++ src/utils/toSentence.ts
@@ -0,0 +1,41 @@
+
+interface SentenceParams {
+    join?: string;
+    joinLast?: string;
+    prepend?: string;
+    append?: string;
+    onEmpty?: string;
+}
+
+const DEFAULT_PARS: SentenceParams  = {
+    join: ', ',
+    joinLast: ' and ',
+    prepend: '',
+    append: '.',
+    onEmpty: 'nothing'
+};
+    
+export function toSentence(
+    arr: string[], 
+    pars?: SentenceParams
+): string {
+        pars = { ...DEFAULT_PARS, ...pars };
+
+        let sentence: string | undefined;
+        switch (arr.length) {
+            case 0:
+                sentence = pars.onEmpty;
+                break;
+            case 1:
+                sentence = arr[0];
+                break;
+            case 2:
+                sentence =  arr.join(pars.join);
+                break;
+            default:
+                const lastItem = arr.pop();
+                sentence = [arr.join(pars.join), lastItem].join(pars.joinLast);
+            }
+
+            return [pars.prepend, sentence, pars.append].join('')
+    }
